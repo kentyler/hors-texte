cff262de1f81bc7828340de0d4a29b45b72971e3
Ken Tyler <ken@8thfold.com>
2025-03-31 10:32:01 -0700

    remove history files from initial commit

--- .history/src/routes/index_20250330214348.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Firestore API' });
-});
-
-export default router;
